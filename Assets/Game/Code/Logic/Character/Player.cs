1ff6c6d3abd0a62dea299202942142f11f1c2786
dovara41 <dovara41@gmail.com>
2025-02-22 14:58:52 +0100

    Player update. Background prefabs

--- Assets/Game/Code/Logic/Character/Player.cs
+++ Assets/Game/Code/Logic/Character/Player.cs
@@ -7,7 +7,8 @@ namespace Game.Code.Logic.Character
         private Rigidbody2D _rb;
         
         private float _horizontal, _vertical;
-        private Vector2 test;
+        private Vector2 _currentVelocity = Vector2.zero;
+        private Vector2 _velocitySmooth;
         
         private void Awake()
         {
@@ -22,15 +23,23 @@ namespace Game.Code.Logic.Character
 
         private void FixedUpdate()
         {
-            var input = new Vector2(_horizontal, _vertical);
-            // var movement = _rb.position + input * (Time.fixedDeltaTime * 3f);
-            var movement = Vector2.SmoothDamp(
-                _rb.position,
-                _rb.position + input * (Time.fixedDeltaTime * 8f),
-                ref test,
-                0.05f);
+            // var input = new Vector2(_horizontal, _vertical);
+            // // var movement = _rb.position + input * (Time.fixedDeltaTime * 3f);
+            // var movement = Vector2.SmoothDamp(
+            //     _rb.position,
+            //     _rb.position + input * (Time.fixedDeltaTime * 8f),
+            //     ref test,
+            //     0.05f);
+            //
+            // _rb.MovePosition(movement);
             
-            _rb.MovePosition(movement);
+            var input = new Vector2(_horizontal, _vertical);
+            var targetVelocity = input.normalized * 1f;
+            
+            _currentVelocity = Vector2.SmoothDamp(_currentVelocity, targetVelocity, ref _velocitySmooth, 0.5f);
+            
+            // _rb.MovePosition(_rb.position + _currentVelocity * Time.fixedDeltaTime);
+            _rb.velocity = _currentVelocity;
         }
     }
 }
