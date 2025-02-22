c12e9a35554721083f07b3d18e5e9c0aacef09eb
keiji <xmkos2006@gmail.com>
2025-02-22 14:02:32 +0100

    Switch to TextMeshPro and 2D physics, update GameObjects for own tests

--- Assets/Game/Code/Logic/interaction_with_objects/TriggerObjectInteraction.cs
+++ Assets/Game/Code/Logic/interaction_with_objects/TriggerObjectInteraction.cs
@@ -1,13 +1,12 @@
 using UnityEngine;
-using UnityEngine.UI;
-using Game.Code;
-using Game.Code.Infrastructure;
+using TMPro; // For TextMeshPro
+using Game.Code; // For Constants
 using Game.Code.Infrastructure.GameObjectsLocator;
+using GameClass = Game.Code.Infrastructure.Game; // Alias for Game class
 
 public class TriggerObjectInteraction : MonoBehaviour
 {
-    [SerializeField] private string interactButton = "E";
-    [SerializeField] private Text interactPrompt;
+    [SerializeField] private TMP_Text interactPrompt;
     [SerializeField] private string promptMessage = "Press E to interact";
 
     private bool isPlayerNearby = false;
@@ -19,36 +18,37 @@ public class TriggerObjectInteraction : MonoBehaviour
             interactPrompt.gameObject.SetActive(false);
         }
     }
-    // during frames updates
+
     private void Update()
     {
-        if (isPlayerNearby && Input.GetButtonDown(interactButton))
+        if (isPlayerNearby && Input.GetKeyDown(KeyCode.E))
         {
-            if (!Game.Instance.Container.Contains(Constants.HudName))
+            if (!GameClass.Instance.Container.Contains(Constants.HudName))
             {
-                Game.Instance.Container.RegisterGameObject(Constants.HudName, Constants.HudPath);
+                GameClass.Instance.Container.RegisterGameObject(Constants.HudName, Constants.HudPath);
             }
 
-            var hud = Game.Instance.Container.GetGameObjectByName<GameObject>(Constants.HudName);
+            var hud = GameClass.Instance.Container.GetGameObjectByName<GameObject>(Constants.HudName);
             hud.SetActive(true);
         }
     }
-    // when player is nearby
-    private void OnTriggerEnter(Collider other)
+
+    // When player is nearby
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             isPlayerNearby = true;
             if (interactPrompt != null)
-            { // dont forget to add action after interaction ( minigame or item obtaining)
-
+            {
                 interactPrompt.text = promptMessage;
                 interactPrompt.gameObject.SetActive(true);
             }
         }
     }
-    // when player is not nearby
-    private void OnTriggerExit(Collider other)
+
+    // When player is not nearby
+    private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
@@ -57,11 +57,11 @@ public class TriggerObjectInteraction : MonoBehaviour
             {
                 interactPrompt.gameObject.SetActive(false);
             }
-            if (Game.Instance.Container.Contains(Constants.HudName))
+            if (GameClass.Instance.Container.Contains(Constants.HudName))
             {
-                var hud = Game.Instance.Container.GetGameObjectByName<GameObject>(Constants.HudName);
+                var hud = GameClass.Instance.Container.GetGameObjectByName<GameObject>(Constants.HudName);
                 hud.SetActive(false);
-                Game.Instance.Container.RemoveGameObject(Constants.HudName);//removing object to prevent memory leak
+                GameClass.Instance.Container.RemoveGameObject(Constants.HudName); // Removing object to prevent memory leak
             }
         }
     }
