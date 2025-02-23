35e30931be4fd68546979b62019f268b52aada19
Artem Melnykov <artemmelnykov29@gmail.com>
2025-02-23 01:18:31 +0100

    Fixed transition between scenes and refactored

--- Assets/Game/Code/Constants.cs
+++ Assets/Game/Code/Constants.cs
@@ -5,9 +5,11 @@ namespace Game.Code
         // Prefab names
         public const string HudName = "Test";
         public const string SpriteFadeName = "SpriteFade";
-        
+        public const string CurtainSceneName = "TransitionController";
+
         // Prefab paths
         public const string HudPath = "Prefabs/Test";
         public const string SpriteFadePath = "Prefabs/SpriteRendererFade";
+        public const string CurtainSceneTransitionPath = "Prefabs/Hud Display/Curtain";
     }
 }
---
+++ Assets/Game/Code/Logic/ObjectsInteraction/TriggerObjectInteraction.cs
@@ -0,0 +1,90 @@
+using TMPro;
+using UnityEngine;
+// For TextMeshPro
+// For Constants
+using GameClass = Game.Code.Infrastructure.Game; // Alias for Game class
+
+namespace Game.Code.Logic.ObjectsInteraction
+{
+    public class TriggerObjectInteraction : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text interactPrompt;
+        [SerializeField] private string promptMessage = "Press E to interact";
+        [SerializeField] private Animator animator; // Reference to the Animator component
+
+        private bool isPlayerNearby = false;
+
+        private void Start()
+        {
+            if (interactPrompt != null)
+            {
+                interactPrompt.gameObject.SetActive(false);
+            }
+        }
+
+        private void Update()
+        {
+            if (isPlayerNearby && Input.GetKeyDown(KeyCode.E))
+            {
+                if (!GameClass.Instance.Container.Contains(Constants.HudName))
+                {
+                    GameClass.Instance.Container.RegisterGameObject(Constants.HudName, Constants.HudPath);
+                }
+
+                var hud = GameClass.Instance.Container.GetGameObjectByName<GameObject>(Constants.HudName);
+                if (hud != null)
+                {
+                    hud.SetActive(true);
+                }
+
+                // Trigger the disappearing animation
+                if (animator != null)
+                {
+                    animator.SetTrigger("Disappear");
+                }
+            }
+        }
+
+        // When player is nearby
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                isPlayerNearby = true;
+                if (interactPrompt != null)
+                {
+                    interactPrompt.text = promptMessage;
+                    interactPrompt.gameObject.SetActive(true);
+                }
+            }
+        }
+
+        // When player leaves the area
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                isPlayerNearby = false;
+                if (interactPrompt != null)
+                {
+                    interactPrompt.gameObject.SetActive(false);
+                }
+                if (GameClass.Instance.Container.Contains(Constants.HudName))
+                {
+                    var hud = GameClass.Instance.Container.GetGameObjectByName<GameObject>(Constants.HudName);
+                    if (hud != null)
+                    {
+                        hud.SetActive(false);
+                        GameClass.Instance.Container.RemoveGameObject(Constants.HudName); // Prevent memory leak
+                    }
+                }
+            }
+        }
+
+        // Method to destroy the game object
+        private void DestroyObject()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
