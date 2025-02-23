635063282233343b5b013471c2cc9cc257ad1f4e
keiji <xmkos2006@gmail.com>
2025-02-23 05:26:31 +0100

    did main menu

--- Assets/Game/Code/Logic/GUI/MenuNavigation.cs
+++ Assets/Game/Code/Logic/GUI/MenuNavigation.cs
@@ -1,5 +1,4 @@
 using Game.Code.Logic.GUI.FadeIn;
-using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,45 +6,37 @@ namespace Game.Code.Logic.GUI.Menu
 {
     public class MenuManager : MonoBehaviour
     {
-        //[SerializeField] private FadeInCanvasGroup fadeInCanvasGroup;
-        [SerializeField] private GameObject creditsPopup;
-
-        private void Start()
-        {
-            creditsPopup.SetActive(false);
-        }
+        public GameObject creditsCanvas;
 
         public void OnStartButtonClicked()
         {
-            Debug.Log("Start button clicked, switching scene.");
-            StartCoroutine(SwitchScene("ExampleScene"));
+            //Debug.Log("Start button clicked, switching scene.");
+            SceneManager.LoadSceneAsync("Game");
         }
 
         public void OnCreditsButtonClicked()
         {
-            Debug.Log("Credits button clicked, showing credits popup.");
-            creditsPopup.SetActive(true);
+            //Debug.Log("Credits button clicked, showing credits popup.");
+            creditsCanvas.SetActive(true);
         }
 
         public void OnExitButtonClicked()
         {
-            Debug.Log("Exit button clicked, attempting to exit game.");
-            StartCoroutine(ExitGame());
-        }
-
-        private IEnumerator SwitchScene(string sceneName)
-        {
-            //yield return fadeInCanvasGroup.FadeOut(); // can't use due to its protection level
-            SceneManager.LoadScene(sceneName);
-            yield return null; // Ensure all code paths return an IEnumerator
-        }
-
-        private IEnumerator ExitGame()
-        {
-            // yield return fadeInCanvasGroup.FadeOut(); // can't use due to its protection level
+            //Debug.Log("Exit button clicked, attempting to exit game.");
             Application.Quit();
-            Debug.Log("Exit game called. Note: Application.Quit() does not work in the editor.");
-            yield return null; // Ensure all code paths return an IEnumerator
+        }
+
+        public void OnHideCreditsButtonClicked()
+        {
+            creditsCanvas.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (creditsCanvas.activeSelf && Input.GetKeyDown(KeyCode.Escape))
+            {
+                OnHideCreditsButtonClicked();
+            }
         }
     }
 }
